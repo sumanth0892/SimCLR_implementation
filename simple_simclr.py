2b92466999fcc3ada133c6da896033bf80492b74
sumanth0892 <sumanth.murthy26@gmail.com>
2025-11-11 14:15:52 +0530

    augmentation for simclr

---
+++ simple_simclr.py
@@ -0,0 +1,13 @@
+# augmentation.py
+import torchvision.transforms as T
+
+def get_simclr_augmentation(img_size=224):
+    """SimCLR augmentation pipeline"""
+    return T.Compose([
+        T.RandomResizedCrop(img_size),
+        T.RandomHorizontalFlip(),
+        T.ColorJitter(0.8, 0.8, 0.8, 0.2),
+        T.RandomGrayscale(p=0.2),
+        T.GaussianBlur(kernel_size=23),
+        T.ToTensor()
+    ])
